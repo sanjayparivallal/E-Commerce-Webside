e0933787a02cc3c24780ff0ae54707d617f344c5
sanjayparivallal <sanjayparivallal@gmail.com>
2024-09-29 00:07:11 +0530

    bug fix

--- collection.js
+++ collection.js
@@ -13,3 +13,10 @@ search.addEventListener("keyup", function(event) {
         }
     }
 });
+var box = document.querySelector(".boxmodel");
+function shownavbar() {
+  box.style.left = "0";
+}
+function closenavbar() {
+  box.style.left = "-60%";
+}
